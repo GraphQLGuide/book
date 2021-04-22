fafc2e9aa5f2ba6e5881f0bfbef218b10557236c
Loren 🤓 <lorensr@gmail.com>
2021-04-22 19:37:00 -0400

    Remove logs & comments

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,27 +1,16 @@
-// A simple, no-op service worker that takes immediate control.
-
-self.addEventListener('install', () => {
-  // Skip over the "waiting" lifecycle state, to ensure that our
-  // new service worker is activated immediately, even if there's
-  // another tab open controlled by our older service worker code.
+self.addEventListener('install', function () {
   self.skipWaiting()
 })
 
-self.addEventListener('activate', () => {
-  // Optional: Get a list of all the current open windows/tabs under
-  // our service worker's control, and force them to reload.
-  // This can "unbreak" any open windows/tabs as soon as the new
-  // service worker activates, rather than users having to manually reload.
-  self.clients.matchAll({ type: 'window' }).then((windowClients) => {
+self.addEventListener('activate', function () {
+  self.clients.matchAll({ type: 'window' }).then(function (windowClients) {
     windowClients.forEach((windowClient) => {
       windowClient.navigate(windowClient.url)
     })
   })
 
   if ('serviceWorker' in navigator) {
-    console.log('ready sw?')
-    navigator.serviceWorker.ready.then((registration) => {
-      console.log('yes sw')
+    navigator.serviceWorker.ready.then(function (registration) {
       registration.unregister().then(() => window.location.reload(true))
     })
   }
