288e190d196e7ae65da840d0a8d6e3f452af5833
Loren 🤓 <lorensr@gmail.com>
2021-05-04 15:20:37 -0400

    Add json/ld Product data

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Helmet } from 'react-helmet'
 
 import './Landing.css'
 import AboveFold from './AboveFold'
@@ -11,6 +12,63 @@ class Landing extends Component {
         {/* <header className="Landing-header">Sign in</header> */}
         <AboveFold />
         <BelowFold />
+        <Helmet>
+          <script type="application/ld+json">
+            {`
+{
+  "@context": "https://schema.org/",
+  "@type": "Product",
+  "name": "The GraphQL Guide",
+  "image": [
+    "https://res.cloudinary.com/graphql/image/upload/c_scale,f_auto,q_90,w_1000/book",
+   ],
+  "description": "A comprehensive book from John Resig, the creator of jQuery. The complete guide to GraphQL, from a beginner introduction to advanced client and server topics.",
+  "review": {
+    "@type": "Review",
+    "reviewRating": {
+      "@type": "Rating",
+      "ratingValue": "5",
+      "bestRating": "5"
+    },
+    "author": {
+      "@type": "Person",
+      "name": "Shawn Wang"
+    },
+    "reviewBody": "This is the most comprehensive coverage of modern GraphQL, going from the basics of the Query Language to excruciating detail on Validation and Execution internals that you’ll be thankful for when things go wrong, to the latest and greatest in the ecosystem, like Apollo Federation and Hasura. It normally takes a developer months to get up to speed on putting together all these pieces. The GraphQL Guide teaches it in just a few hours!"
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "5",
+    "reviewCount": "7"
+  },
+  "offers": {
+    "@type": "Offer",
+    "url": "https://graphql.guide/#pricing",
+    "priceCurrency": "USD",
+    "price": "39",
+    "availability": "https://schema.org/InStock",
+    "name": "The GraphQL Guide (Basic)"
+  },
+  "offers": {
+    "@type": "Offer",
+    "url": "https://graphql.guide/#pricing",
+    "priceCurrency": "USD",
+    "price": "89",
+    "availability": "https://schema.org/InStock",
+    "name": "The GraphQL Guide (Pro)"
+  },
+  "offers": {
+    "@type": "Offer",
+    "url": "https://graphql.guide/#pricing",
+    "priceCurrency": "USD",
+    "price": "289",
+    "availability": "https://schema.org/InStock",
+    "name": "The GraphQL Guide (Full)"
+  }
+}
+`}
+          </script>
+        </Helmet>
       </div>
     )
   }
